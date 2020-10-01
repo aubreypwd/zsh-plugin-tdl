d9f7193e6867372b16de9c6c6969ff4d689f86fe
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2020-10-01 11:27:45 -0600

    Add the function.

--- plugin-slug.plugin.zsh
+++ plugin-slug.plugin.zsh
@@ -1,7 +1,29 @@
 #!/bin/zsh
 
 ###
- # My Plugin
+ # Watch debug.log
  #
+ # @since 6/21/16
  # @since 1.0.0
  ##
+function tdl {
+	if [[ ! $(pwd) == *"content"* ]]; then
+		echo "Not in wp-content." && return
+	fi
+
+	if ! [ -e ./debug.log ]; then
+		touch "debug.log"
+	fi
+
+ if [[ "$@" == *"--clear"* ]]; then
+  clear
+ fi
+
+ 	if ! [[ -x $(command -v rainbow) ]]; then
+		 echo "Please install rainbow:";
+		 echo "\tHomebrew: brew install rainbow"
+		 return
+	 fi
+
+	rainbow --red="error|Fatal|fatal|Error|PHP\s Fatal|WordPress\sdatabase\serror" --yellow="warning|PHP\sWarning|PHP\sNotice" --magenta="line\s[0-9]+" --green="\/Users\/.*\.php" --blue="^\[.*\]" tail -f debug.log
+}
